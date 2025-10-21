2cb4e74411f9f49accbe083f6dbc3dd3337831f1
francesco.barbera <barberaf84@gmail.com>
2025-10-21 15:43:25 +0200

    add fake initial results

---
+++ src/initial-results.ts
@@ -0,0 +1,26 @@
+import type { AthleteRaceResult } from "./entities/athlete-race-result";
+
+export const initialResults: AthleteRaceResult[] = [
+  {
+    id: "97904f49-3d6d-4ed6-9f6c-c1800e0f63cb",
+    athlete: {
+      id: "336165cc-66e8-47af-b5f2-6bfcd120b207",
+      firstName: "Giovanni",
+      lastName: "Panetta",
+      gender: "M",
+      year: 1971,
+      category: "SENIOR",
+      bibNumber: 101,
+      sportsClub: "CUS Torino",
+      position: 1,
+    },
+    race: {
+      id: "5afc7b75-bacb-4d1c-94c8-50d69da39822",
+      name: "Gara Storica",
+      date: new Date("2025-01-22"),
+    },
+    timeSeconds: 3607,
+    gapSeconds: 0,
+    paceMinKm: 6.01,
+  },
+];
